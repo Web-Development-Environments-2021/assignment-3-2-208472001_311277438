cff998b57bf3871c094d268e60a7f4f23a9246bd
Shuhhh <63115888+Shuhhh@users.noreply.github.com>
2021-05-25 16:37:51 +0300

    rtth

--- project/routes/games.js
+++ project/routes/games.js
@@ -5,7 +5,7 @@ const DButils = require("../routes/utils/DButils");
 router.post("/addGame", async (req, res, next) => {
   try{
     await DButils.execQuery(
-      `INSERT INTO dbo.games (gameID, gamedate, gametime, hometeam, awayteam, field, score) VALUES ('${req.body.gameID}', '${req.body.gamedate}','${req.body.gametime}', '${req.body.hometeam}','${req.body.awayteam}','${req.body.field}','${req.body.score}')`
+      `INSERT INTO dbo.games (gameID, gamedate, gametime, hometeamID, awayteamID, field, score) VALUES ('${req.body.gameID}', '${req.body.gamedate}','${req.body.gametime}', '${req.body.hometeamID}','${req.body.awayteamID}','${req.body.field}','${req.body.score}')`
     );
   res.status(201).send("game has been added");
 } catch (error) {
@@ -16,7 +16,7 @@ router.post("/addGame", async (req, res, next) => {
 router.post("/addEvent", async (req, res, next) => {
     try{
       await DButils.execQuery(
-        `INSERT INTO dbo.events (gameID, eventdate, eventtime, eventminute, dataevent, player) VALUES ('${req.body.gameID}', '${req.body.eventdate}','${req.body.eventtime}', '${req.body.eventminute}','${req.body.dataevent}','${req.body.field}','${req.body.player}')`
+        `INSERT INTO dbo.events (gameID, eventdate, eventtime, eventminute, dataevent, playerID) VALUES ('${req.body.gameID}', '${req.body.eventdate}','${req.body.eventtime}', '${req.body.eventminute}','${req.body.dataevent}','${req.body.field}','${req.body.playerID}')`
       );
     res.status(201).send("event has been added");
   } catch (error) {
--- project/routes/teams.js
+++ project/routes/teams.js
@@ -1,28 +1,31 @@
 var express = require("express");
+const { VarChar } = require("mssql");
 var router = express.Router();
 const DButils = require("./utils/DButils");
 const players_utils = require("./utils/players_utils");
 let teamID;
 
-
 router.get("/teamFullDetails/:teamId", async (req, res, next) => {
   let team_details = [];
-  teamID = req.params.teamid;
+  teamID = req.params.teamId;
   try {
-    const team_details = await players_utils.getPlayersByTeam(
-      req.params.teamId
-    );
+    // const players_details = await players_utils.getPlayersByTeam(teamID);
+    // team_details.push(players_details);
     //we should keep implementing team page.....
+
+    past_home_team_games = await DButils.execQuery(`SELECT * FROM dbo.games WHERE hometeamID = ${teamID} AND score IS NOT NULL`);
+    past_away_team_games = await DButils.execQuery(`SELECT * FROM dbo.games WHERE awayteamID = ${teamID} AND score IS NOT NULL`);
+    future_home_team_games = await DButils.execQuery(`SELECT * FROM dbo.games WHERE hometeamID = ${teamID} AND score IS NULL`);
+    future_away_team_games = await DButils.execQuery(`SELECT * FROM dbo.games WHERE awayteamID = ${teamID} AND score IS NULL`);
+    team_details.push(past_home_team_games);
+    team_details.push(past_away_team_games);
+    team_details.push(future_home_team_games);
+    team_details.push(future_away_team_games);
+
     res.send(team_details);
   } catch (error) {
     next(error);
   }
 });
-
-
-// add the new username
-// const team_games = DButils.execQuery(`SELECT * FROM dbo.games WHERE hometeam = '${teamID}'`)[0];
-
-// console.log(team_games);
 
 module.exports = router;
