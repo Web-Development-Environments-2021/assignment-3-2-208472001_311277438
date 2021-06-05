cb0227bb198e98932b98ea538857e949c474a4dc
amit yosefi <amit.yosefi@gmail.com>
2021-06-05 18:56:44 +0300

    manager finished

--- project/routes/utils/users_utils.js
+++ project/routes/utils/users_utils.js
@@ -2,22 +2,37 @@ const DButils = require("./DButils");
 
 
 async function markAsFavorite(table, user_id, id) {
+  if (table == "Game"){
+    const row = await DButils.execQuery(
+      `SELECT gameid, homegoal FROM dbo.${table}s WHERE gameID = ${id}`
+    );
+
+    if (typeof row[0] === 'undefined'){
+      throw { status: 201, message: `there is no ${table} with this id` };
+    }
+    if (typeof row[0].homegoal != null){
+      throw { status: 201, message: `there game already played` };
+    }
+  }
+
+  const index = await DButils.execQuery(
+  `select ${table}id from dbo.favorite${table}s where ${table}id = ${id} and userid = ${user_id}`
+  );
+
+  if (typeof index[0] != 'undefined'){
+    throw { status: 201, message: `the ${table} already in user ${user_id} favorites` };
+  }
+  
   await DButils.execQuery(
-    `insert into favorite${table} values ('${user_id}',${id})`
+    `insert into dbo.favorite${table}s values ('${user_id}','${id}')`
   );
 }
 
 async function getFavorite(table, user_id) {
   const ids = await DButils.execQuery(
-    `select top 3 ${table}ID from favorite${table}s where userID=${user_id}`
+    `select top 3 ${table}ID from dbo.favorite${table}s where userID=${user_id}`
   );
   return ids;
-}
-
-async function getFavoritegameDetails(game_id) {
-  const game_details = await DButils.execQuery(`select * from dbo.Games where gameID='${game_id}'`
-  );
-  return game_details;
 }
 
 
